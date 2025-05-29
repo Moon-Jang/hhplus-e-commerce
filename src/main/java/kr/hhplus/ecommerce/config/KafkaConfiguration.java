e53ea24bc3049dbc66c3253ece6057bbb7c2d33e
Moon-Jang <roby8502@naver.com>
2025-05-29 11:43:22 +0900

    [feat] 카프카 프로듀서 컨슈머 설정

--- src/main/java/kr/hhplus/ecommerce/config/KafkaConfiguration.java
+++ src/main/java/kr/hhplus/ecommerce/config/KafkaConfiguration.java
@@ -3,11 +3,13 @@ package kr.hhplus.ecommerce.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.annotation.EnableKafkaRetryTopic;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.support.converter.StringJsonMessageConverter;
 
 @EnableKafka
+@EnableKafkaRetryTopic
 @Configuration
 public class KafkaConfiguration {
     @Bean
